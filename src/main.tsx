7a3b28bf2f35c189f85543bb401c3e52bd33a9a1
abdo-aleer <juniorab444@gmail.com>
2026-05-31 14:45:46 +0200

    chore: configured vite-plugin-pwa

--- src/main.tsx
+++ src/main.tsx
@@ -9,15 +9,3 @@ createRoot(document.getElementById('root')!).render(
   </StrictMode>,
 )
 
-// if (import.meta.env.PROD && "serviceWorker" in navigator) {
-//   window.addEventListener("load", () => {
-//     navigator.serviceWorker
-//       .register("/sw.js")
-//       .then((_) => {
-//         console.log("Service Worker registered successfully:");
-//       })
-//       .catch((error) => {
-//         console.error("Service Worker registration failed:", error);
-//       });
-//   });
-// }
